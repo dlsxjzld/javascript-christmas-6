1e2002b02f21e3a07085b561dfd5c9011b16fde2
dlsxjzld <sinjuk1@naver.com>
2024-12-10 18:24:49 +0900

    feat: test 통과할 수 있도록 예외 처리 메시지 수정

--- src/validation/validateFunctions.js
+++ src/validation/validateFunctions.js
@@ -9,17 +9,23 @@ export const toThrowNewError = (condition, errorMessage) => {
 };
 
 const hasEmptySpace = (input) => [
-  toThrowNewError(input.includes(' '), '공백은 안됩니다.'),
+  toThrowNewError(
+    input.includes(' '),
+    '유효하지 않은 날짜입니다. 다시 입력해 주세요. 공백은 안됩니다.',
+  ),
 ];
 
 const isEmptyString = (input) => {
-  toThrowNewError(input === '', '빈 문자열은 안됩니다.');
+  toThrowNewError(
+    input === '',
+    '유효하지 않은 날짜입니다. 다시 입력해 주세요. 빈 문자열은 안됩니다.',
+  );
 };
 
 const isNumberType = (input) => {
   toThrowNewError(
     Number.isInteger(Number(input)) === false,
-    '숫자만 가능합니다. NaN과 Infinity는 안됩니다.',
+    '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
   );
 };
 
@@ -43,7 +49,7 @@ const isOrderCountRight = (input) => {
   const commaCount = input.split('').filter((char) => char === ',').length;
   toThrowNewError(
     orderCount - 1 !== commaCount,
-    ',는 주문과 주문 사이에 1개만 존재해야 합니다.',
+    '유효하지 않은 주문입니다. 다시 입력해 주세요. ,는 주문과 주문 사이에 1개만 존재해야 합니다.',
   );
 };
 
@@ -70,7 +76,7 @@ const isAllMenuDrink = (input) => {
   const orders = input.split(',');
   toThrowNewError(
     orders.every((order) => isDrink(order)),
-    '메뉴가 전부 음료입니다. 다시 입력해 주세요.',
+    '유효하지 않은 주문입니다. 다시 입력해 주세요.',
   );
 };
 
