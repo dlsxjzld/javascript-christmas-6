4a233a3ee7bb8bd15215385c193b7bde427c4515
dlsxjzld <sinjuk1@naver.com>
2024-12-10 15:32:49 +0900

    feat: 예상 방문 날짜 입력 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import { OutputView } from './view/OutputView.js';
 import { MESSAGE } from './constants/message.js';
 
 class App {
-  async run() {}
+  async run() {
+    OutputView.printResult(MESSAGE.EVENT_PLANNER_INSTRUCTION);
+    const input = await this.getVisitDate();
+    console.log('input', input);
+  }
+
+  async getVisitDate() {
+    try {
+      const input = await InputView.readUserInput(MESSAGE.ASK_VISIT_DATE);
+      const validate = (input) => true;
+      validate(input);
+      return input;
+    } catch (error) {
+      OutputView.printResult(error.message);
+      return this.getVisitDate();
+    }
+  }
 }
 
 export default App;
